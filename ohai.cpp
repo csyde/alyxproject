d5361bb97c02b47a591e84b2fa04c149138ebf7c
csyde <connorbd@yahoo.com>
2014-12-26 17:23:48 -0500

    Delete ohai.cpp

--- ohai.cpp
+++
@@ -1,8 +0,0 @@
-//Test file for C compile command.
-//Contains offensive Irish stereotype. Do not read, compile, or run.
-
-#include <stdio.h>
-
-int main() {
-	printf("feck off, ye gobshites!\n");
-}
